2eb52c2f3f61c231bec866f01a6f21bf898bda9b
edgardopina <edgardopina@yahoo.com>
2021-08-15 19:24:45 -0600

    Added edit, delete, and select actions to task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,87 @@
 var formElement = document.querySelector("#task-form");
 var tasksToDoElement = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;	// unique task id
 
-var taskFormHandler = function (event) {
-	event.preventDefault(); // overides legacy browser behavior that submit uploads form to server
+var createTaskActions = function (taskId) {
+	var actionContainerElement = document.createElement("div"); // creater container <div> element
+	actionContainerElement.className = "task-actions"; // styles <div> element
 
-	var taskNameInput = document.querySelector("input[name='task-name']").value;
-	var taskTypeInput = document.querySelector("select[name='task-type']").value;
+	// create edit button
+	var editButtonElement = document.createElement("button");
+	editButtonElement.textContent = "Edit";
+	editButtonElement.className = "btn edit-btn";
+	editButtonElement.setAttribute("data-task-id", taskId);
+	actionContainerElement.appendChild(editButtonElement); // adds Edit button to <div> element
 
-	// check if input values is empty strings
-	if (!taskNameInput || !taskTypeInput) {
-		window.alert("You need to fill out the task form!");
-		return;
+	// create delete button
+	var deleteButtonElement = document.createElement("button");
+	deleteButtonElement.textContent = "Delete";
+	deleteButtonElement.className = "btn delete-btn";
+	deleteButtonElement.setAttribute("data-task-id", taskId);
+	actionContainerElement.appendChild(deleteButtonElement); // adds Delete button to <div> element
+
+	// add dropdown to select task status
+	var statusSelectElement = document.createElement("select");
+	statusSelectElement.className = "select-status";
+	statusSelectElement.setAttribute("name", "status-change");
+	statusSelectElement.setAttribute("data-task-id", taskId);
+	actionContainerElement.appendChild(statusSelectElement); // adds empty <select> to <div> element
+
+	var statusChoices = ["To Do", "In Progress", "Completed"];	// adds options to empty select
+	var statusChoicesLength = statusChoices.length;
+	for (var i = 0; i < statusChoicesLength; i++) {
+		var statusOptionElement = document.createElement("option"); // create option element
+		statusOptionElement.setAttribute("value", statusChoices[i]);
+		statusOptionElement.textContent = statusChoices[i];
+		statusSelectElement.appendChild(statusOptionElement); // append to select
 	}
 
-	formElement.reset();
-	// pack up data as an object
-	var taskDataObj = {
-		name: taskNameInput,
-		type: taskTypeInput,
-	};
-
-	createTaskElement(taskDataObj); // send it as an argument to createTaskEl
+	return actionContainerElement; // returns <div> element with Edit button, Delete button, and Select option dropdown
 };
 
 var createTaskElement = function (taskDataObj) {
 	var listItemElement = document.createElement("li"); // create list item
 	listItemElement.className = "task-item";
 
+	listItemElement.setAttribute("data-task-id", taskIdCounter); // add task id as a custom attribute
+
 	var taskInfoElement = document.createElement("div"); // create div to hold task info and add to list item
 	taskInfoElement.className = "task-info";
-	// add HTML content to div
+	// add task name and type to div
 	taskInfoElement.innerHTML =
 		"<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
-	listItemElement.appendChild(taskInfoElement);
-	tasksToDoElement.appendChild(listItemElement);
+	listItemElement.appendChild(taskInfoElement); // adds task to list-item
+
+	var taskActionElement = createTaskActions(taskIdCounter); // builds edit/delete/status actions 
+	
+	listItemElement.appendChild(taskActionElement); // adds actions to list item
+	tasksToDoElement.appendChild(listItemElement); //adds list item to TO DO list 
+
+	taskIdCounter++; // increase task counter for next unique task id
+};
+
+var taskFormHandler = function (event) {
+	event.preventDefault(); // overides legacy browser behavior that submit uploads form to server
+	// gets input values
+	var taskNameInput = document.querySelector("input[name='task-name']").value;
+	var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+	// validates and messages error condition when input values are empty strings
+	if (!taskNameInput || !taskTypeInput) {
+		window.alert("You need to fill out the task form!");
+		// return false;
+		return; // resturn to eventListener
+	}
+
+	formElement.reset(); // resets form element
+	// pack up data as an object
+	var taskDataObj = {
+		name: taskNameInput,
+		type: taskTypeInput,
+	};
+
+	createTaskElement(taskDataObj); // create tak element for taskDataObj
 };
 
 formElement.addEventListener("submit", taskFormHandler);
